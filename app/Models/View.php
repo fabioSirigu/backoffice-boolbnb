42ddfabbdba1648b77664afb0654057333c850e8
manuelgaleone <galeonemanuel14@gmail.com>
2023-02-03 16:41:30 +0100

    Add message and view models, realtions and migrate

--- app/Models/View.php
+++ app/Models/View.php
@@ -6,10 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Message extends Model
+class View extends Model
 {
-
-    protected $fillable = ['name', 'homes_id', 'email', 'message'];
+    protected $fillable = ['user_ip', 'homes_id', 'date'];
 
     /**
      * Get the type that owns the Project

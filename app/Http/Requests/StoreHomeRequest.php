e969504b359353216d44a20163884f3c014e22d4
manuelgaleone <galeonemanuel14@gmail.com>
2023-02-03 12:36:45 +0100

    Add validation rules for the datas

---
+++ app/Http/Requests/StoreHomeRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreHomeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|unique:homes,title',
+            'slug' => 'required|max:255',
+            'rooms' => 'required|min:1',
+            'beds' => 'required|max:1|max:25',
+            'bathrooms' => 'required|max:1|max:255',
+            'address' => 'required|min:5|max:255',
+            'latitude' => 'required|nullable|min:5|max:100',
+            'longitude' => 'required|nullable|min:5|max:100',
+            'cover_image' => 'required|image|max:300',
+            'visible' => 'required|boolean'
+        ];
+    }
+}

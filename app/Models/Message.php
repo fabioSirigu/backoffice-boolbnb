19bcbe3ed2a6ce1a917fc501bc0900f385a9230b
manuelgaleone <galeonemanuel14@gmail.com>
2023-02-03 16:31:55 +0100

    Make foreign key migration and relationship

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,11 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Message extends Model
 {
 
     protected $fillable = ['name', 'email', 'message'];
+
+    /**
+     * Get the type that owns the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function home(): BelongsTo
+    {
+        return $this->belongsTo(Home::class);
+    }
 
     use HasFactory;
 }

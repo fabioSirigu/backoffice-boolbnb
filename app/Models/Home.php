4d9b837e83ee36a8c410ee6cef7d9fd743ab1a6b
manuelgaleone <galeonemanuel14@gmail.com>
2023-02-03 12:42:31 +0100

    Add protected fillable in model and slug function

--- app/Models/Home.php
+++ app/Models/Home.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Home extends Model
 {
+    protected $fillable = ['title', 'slug', 'rooms', 'beds', 'bathrooms', 'address', 'latitude', 'longitude', 'cover_image', 'visible'];
+
+    public static function createSlug($title)
+    {
+        $home_slug = Str::slug($title);
+        return $home_slug;
+    }
+
     use HasFactory;
 }

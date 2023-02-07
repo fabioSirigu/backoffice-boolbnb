27b49334abae05133c3c19cd9b63c4e1f3ec7c5b
Yuri-Punzo <yuripunzo@hotmail.it>
2023-02-07 16:23:28 +0100

    temporarily hidden services and sponsored crud

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -73,7 +73,7 @@
                                     Homes
                                 </a>
                             </li>
-                            <li class="nav-item">
+                            <!-- <li class="nav-item">
                                 <a class="nav-link" href="{{route ('admin.services.index')}}">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file align-text-bottom" aria-hidden="true">
                                         <path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path>
@@ -90,7 +90,7 @@
                                     </svg>
                                     Sponsored
                                 </a>
-                            </li>
+                            </li> -->
                     </div>
                 </nav>
 

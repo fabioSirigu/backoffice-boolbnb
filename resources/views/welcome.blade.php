1eabea2a97903e05d97efc0e358b6f4f84848103
naz1ne <fabio.sirigu.1995@gmail.com>
2023-02-09 14:36:15 +0100

    fix bug welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,16 @@
 <div class="jumbotron p-5 bg-light rounded-3">
     <div class="container py-5">
         <h2><img src="/img/logoBnBlateral.png" alt="" class="logo_lateral"></h2>
-        <h1>Benvenuto in BoolBnB</h1>
+        @if (Auth::user())
+        <h1>Benvenuto <strong>{{ Auth::user()->name }} </strong> in BoolBnB</h1>
+        @else
+        <h1>Benvenuto in BoolBnB, fai il login o registrati!</h1>
+        @endif
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis reprehenderit accusantium facere id aliquid iure dignissimos doloremque ab at molestiae. Adipisci modi debitis deleniti cupiditate ab in quibusdam voluptatibus nam. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius totam, laboriosam esse minima architecto blanditiis deserunt tenetur aut voluptatibus molestiae ab quidem tempora, quis commodi praesentium? Magnam eos eaque enim!
         </p>
+        @if (Auth::user())
+        <a href="{{route('admin.homes.index')}}" class="btn btn-primary">View All Homes</a>
+        @endif
     </div>
 </div>
 

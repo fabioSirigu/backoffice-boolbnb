0013c7979c59bbd2c805189c051ff436b1a02607
naz1ne <fabio.sirigu.1995@gmail.com>
2023-02-10 12:56:23 +0100

    add label cover image on edit

--- resources/views/admin/homes/edit.blade.php
+++ resources/views/admin/homes/edit.blade.php
@@ -24,20 +24,20 @@
             <label for="title" class="form-label">Title*</label>
             <input type="text" name="title" id="title" class="form-control" placeholder="Nome" value="{{old('title', $home->title)}}" aria-describedby="helpId" required>
         </div>
-        <div class="mb-3 d-flex gap-2">
-            <div>
+        <div class="mb-3 d-flex gap-2 justify-content-between gap-3">
+            <div class="flex-grow-1">
                 <label for="rooms" class="form-label">Rooms*</label>
                 <input type="number" name="rooms" id="rooms" class="form-control" placeholder="Stanze" value="{{old('rooms', $home->rooms)}}" aria-describedby="helpId" required>
             </div>
-            <div>
+            <div class="flex-grow-1">
                 <label for="beds" class="form-label">Beds*</label>
                 <input type="number" name="beds" id="beds" class="form-control" placeholder="Letti" value="{{old('beds', $home->beds)}}" aria-describedby="helpId" required>
             </div>
-            <div>
+            <div class="flex-grow-1">
                 <label for="bathrooms" class="form-label">Bathrooms*</label>
                 <input type="number" name="bathrooms" id="bathrooms" class="form-control" placeholder="Bagni" value="{{old('bathrooms', $home->bathrooms)}}" aria-describedby="helpId" required>
             </div>
-            <div>
+            <div class="flex-grow-1">
                 <label for="square_meters" class="form-label">Square Meters*</label>
                 <input type="number" name="square_meters" id="square_meters" class="form-control" placeholder="Metri quadrati" value="{{old('square_meters', $home->square_meters)}}" aria-describedby="helpId" required>
             </div>
@@ -78,7 +78,10 @@
         </div>
         <div class="mb-3 d-flex align-items-center gap-4">
             <img width="200px" src="{{asset('storage/' . $home->cover_image)}}">
-            <input type="file" class="form-control" name="cover_image" id="cover_image" placeholder="Aggiungi un'immagine" aria-describedby="coverImgHelper">
+            <div class="w-100">
+                <label for="cover_image" class="form-label">Cover Image*</label>
+                <input type="file" class="form-control" name="cover_image" id="cover_image" placeholder="Aggiungi un'immagine" aria-describedby="coverImgHelper">
+            </div>
         </div>
         <p>
             <em>

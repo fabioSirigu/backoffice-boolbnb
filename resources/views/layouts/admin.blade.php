dd0d4a1404339c77a5d032276da633d2c20fd12c
manuelgaleone <galeonemanuel14@gmail.com>
2023-02-12 22:54:28 +0100

    Add admin title and flav

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,11 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>
+        BackOffice - BoolBnb
+    </title>
+    
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('img/favIcon.png') }}">
 
     <!-- FONT AWESOME -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />

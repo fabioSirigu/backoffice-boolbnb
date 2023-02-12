40e1303cbf2f70a636c1dbb58d2002c9c293cb13
manuelgaleone <galeonemanuel14@gmail.com>
2023-02-12 22:46:54 +0100

    Fixed flavicon and titles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,11 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>
+        BackOffice - BoolBnb
+    </title>
 
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('img/favIcon.png') }}">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">

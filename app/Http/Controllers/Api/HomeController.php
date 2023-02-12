b282ff6513c11a8e8e9a1a76b4c478267d4d5d28
manuelgaleone <galeonemanuel14@gmail.com>
2023-02-12 22:39:55 +0100

    Add show function in api controller

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -11,9 +11,33 @@ class HomeController extends Controller
     public function index()
     {
         $homes = Home::with('services')->get();
-        return response()->json([
-            'success' => true,
-            'data' => $homes
-        ]);
+        if ($homes) {
+            return response()->json([
+                'success' => true,
+                'data' => $homes
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'error' => 'Nessuna Casa trovata.'
+            ]);
+        }
+    }
+
+    public function show($slug)
+    {
+        $homes = Home::with('services')->where('slug', $slug)->first();
+
+        if ($homes) {
+            return response()->json([
+                'success' => true,
+                'data' => $homes
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'error' => 'Nessuna Casa trovata.'
+            ]);
+        }
     }
 }
